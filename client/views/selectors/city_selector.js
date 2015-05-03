722c740b5f319fbf2696855840c849d6257cd9dd
Merlin Patterson <mikepatt77@gmail.com>
2015-05-03 01:59:00 -0400

    Add city_selctor views

---
+++ client/views/selectors/city_selector.js
@@ -0,0 +1,22 @@
+Template.citySelector.helpers({
+  cities: [
+    'Baltimore',
+    'Birmingham',
+    'Charlotte',
+    'Cincinnati',
+    'Cleveland',
+    'Columbus',
+    'Denver',
+    'Detroit',
+    'Indianapolis',
+    'Nashville',
+    'Pittsburgh',
+    'Las Vegas',
+    'Miami',
+    'New Orleans',
+    'Philadelphia',
+    'Providence',
+    'San Antonio',
+    'St. Louis',
+  ]
+});
